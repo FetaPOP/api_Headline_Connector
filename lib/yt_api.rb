e4bff283c20f93f708d68e4f06cb17cd05dc38bc
emily <emiliyvicki601@gmail.com>
2021-10-16 20:18:54 +0800

    Add yt_api.rb

---
+++ lib/yt_api.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require 'http'
+require_relative 'video'
+require_relative 'channel'
+
+module HeadlineConnector
+  # Library for Github Web API
+  class YoutubeApi
+    API_KEY = 'AIzaSyAXaC7TITl7pHR5dp0Si2E_hrFyxNpQTUo'
+    API_PROJECT_ROOT = 'https://youtube.googleapis.com/youtube/v3/'
+    module Errors
+      class NotFound < StandardError; end
+      class Unauthorized < StandardError; end # rubocop:disable Layout/EmptyLineBetweenDefs
+    end
+
+    HTTP_ERROR = {
+      401 => Errors::Unauthorized,
+      404 => Errors::NotFound
+    }.freeze
+
+    def initialize(token)
+      @ACCESS_TOKEN = token
+    end
+
+    def data_collect(id)
+        req_url = yt_path(id)
+        video_data = call_yt_url(req_url).parse
+    end
+
+    def video(data)
+        Video.new(video_data, self)
+
+    end
+
+    def channel(data)
+        Channel.new(channel_data, self)
+    end
+
+    private
+
+    def yt_path(path)
+      "#{API_PROJECT_ROOT}videos?part=snippet&id=#{path}&key=#{API_KEY}"
+    end
+
+    def yt_channel_path(path)
+      "#{API_PROJECT_ROOT}channels?part=snippet&id=#{path}&key=#{API_KEY}"
+    end
+
+    def call_yt_url(url)
+      result =
+        HTTP.headers('Accept' => 'application/json')
+            .get(url)
+      successful?(result) ? result : raise(HTTP_ERROR[result.code])
+    end
+
+    def successful?(result)
+      !HTTP_ERROR.keys.include?(result.code)
+    end
+
+  end
+end
