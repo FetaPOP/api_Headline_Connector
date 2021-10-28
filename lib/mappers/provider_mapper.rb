c85bab6af67140e14c199ae90f22ee3f9b6539d2
emily <emilyvicki601@gmail.com>
2021-10-28 14:59:50 +0800

    refactor: data mapper and remove necessary files

---
+++ lib/mappers/provider_mapper.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+module HeadlineConnector
+  module Youtube
+    # Youtube Mapper: YoutubeApi -> provider entity
+    class ProviderMapper
+      def initialize(api_key, gateway_class = Youtube::Api)
+        @api_key = api_key
+        @gateway_class = gateway_class
+        @gateway = @gateway_class.new(@api_key)
+      end
+
+      def find(id)
+        data = @gateway.collect_data(id)
+        build_entity(data)
+      end
+
+      def build_entity(data)
+        DataMapper.new(data).build_entity
+      end
+
+      # Extracts entity specific elements from data structure
+      class DataMapper
+        def initialize(data)
+          @data = data
+        end
+
+        def build_entity
+          HeadlineConnector::Entity::Provider.new(
+            id: id,
+            title: title
+          )
+        end
+
+        def id
+          @provider['items'][0]['snippet']['channelId']
+        end
+    
+        def title
+          @provider['items'][0]['snippet']['channelTitle']
+        end
+    end
+  end
+end
