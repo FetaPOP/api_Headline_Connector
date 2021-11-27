ce48ef9499e40dc13bae3b318585844cf6836dc9
emily <emilyvicki601@gmail.com>
2021-11-27 21:51:16 +0800

    refactor: reorganize test folder

--- spec/tests/integration/layers/gateway_database_spec.rb
+++ spec/tests/integration/layers/gateway_database_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: false
 
-require_relative 'helpers/spec_helper'
-require_relative 'helpers/vcr_helper'
-require_relative 'helpers/database_helper'
+require_relative '../../../helpers/spec_helper'
+require_relative '../../../helpers/vcr_helper'
+require_relative '../../../helpers/database_helper'
 
 describe 'Integration Tests of Youtube API and Database' do
   VcrHelper.setup_vcr
 
   before do
-    VcrHelper.configure_vcr_for_youtube(recording: :none)
+    VcrHelper.configure_vcr_for_youtube
   end
 
   after do
